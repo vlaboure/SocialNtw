00689d64b97b6982156f0708916ccbd771fcb2e1
VINCENT LABOURE <v.laboure@gmail.com>
2020-08-26 13:54:46 +0200

    first commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {            
+        }
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // pour ajouter les valeurs à la création de migration
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "101" },
+                    new Value { Id = 2, Name = "102" },
+                    new Value { Id = 3, Name = "103" }
+                );
+        }
+    }
+}
